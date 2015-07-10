44920b66f401af92347b4624b28767d944fc86f1
Nassos Antoniou <nassos@thoughtworks.com>
2015-07-10 14:39:36 +0100

    Extend run script

--- run_auth.sh
+++ run_auth.sh
@@ -1,4 +1,58 @@
 #!/bin/bash
 
-vagrant up --no-provision && vagrant provision &
-vagrant rsync-auto
+PACKAGE_DEPENDENCIES=( ansible-playbook vagrant )
+DIRS=( keys certs )
+_V=0
+
+usage() {
+  cat <<EOF
+    Usage: $0 [options] environment
+    -h    Display help message
+    -v    Verbose mode
+EOF
+exit 1;
+}
+
+log () {
+  if [[ $_V -eq 1 ]]; then
+      echo "$@"
+      echo ""
+  fi
+}
+
+check_for_dependencies() {
+  for d in "${PACKAGE_DEPENDENCIES[@]}";
+  do
+    command -v  $d >/dev/null 2>&1 || { echo >&2 "$d binary not found. Please make sure it is installed."; exit 1; }
+  done
+}
+
+launch_auth() {
+  log "Bringing VM machine up"
+  vagrant up --no-provision 
+  log "Bringing VM machine up"
+  vagrant rsync-auto &
+  log "Starting provisioning of Authority Server"
+  vagrant provision
+}
+
+create_dirs() {
+  log "Creating directories $DIRS"
+  mkdir -p $DIRS
+}
+
+while getopts "hv" opt; do
+  case $opt in
+    h)
+      usage
+      exit 0
+      ;;
+    v)
+      _V=1
+      ;;
+  esac
+done
+
+
+check_for_dependencies
+launch_auth
