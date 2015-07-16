39aff0352a9ce4ec2c84513f3a4e4c0e3ead3788
Nassos Antoniou <nassos@thoughtworks.com>
2015-07-16 10:33:59 +0100

    Interim

--- authority_server/roles/agora-tools/templates/export_key.sh
+++ authority_server/roles/agora-tools/templates/export_key.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mkdir -p {{ key_mount_path }}
-eopeers --show-mine | tee {{ key_mount_path }}/{{ own_key_name }} >/dev/null
-cp {{ key_mount_path }}/{{ own_key_name }} {{ certs_dest }}/{{ own_key_name }}
+eopeers --show-mine | tee {{ own_key_name }} {{ certs_dest }}/{{ own_key_name }} >/dev/null
